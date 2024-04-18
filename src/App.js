26dba7a45d2f54e653bdc3112418a772f671891d
Kartik Vijay <kartikvijay143@gmail.com>
2024-04-18 14:02:33 +0530

    tools

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './App.css'; // Import any necessary CSS file for styling
+
+function App() {
+  // State variables to track toggle states and slider value
+   
+  const [sliderValue, setSliderValue] = useState(50);
+   // Initial value for the slider
+   const [button1Active, setButton1Active] = useState(false);
+   const [button2Active, setButton2Active] = useState(false);
+ 
+   const handleButton1Click = () => {
+     setButton1Active(!button1Active);
+     
+   };
+ 
+   const handleButton2Click = () => {
+    setButton2Active(!button2Active);
+    
+  };
+  
+   
+
+  
+
+  // Function to handle slider value change
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  };
+
+  return (
+    <div className='container'>
+      
+      <div className="blackboard">
+        <div className="controls">
+          <div>
+          <button className={`toggle ${button1Active ? 'active' : ''}`} onClick={handleButton1Click}></button>
+        </div>
+        <div>
+        <button className={`toggle ${button2Active ? 'active' : ''}`} onClick={handleButton2Click}></button> 
+        </div>
+      </div>
+
+      <div className="sliderdiv">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={sliderValue}
+          onChange={handleSliderChange}
+          style={{ opacity: sliderValue / 200 + 0.5 }}
+          className='slider'
+        />
+      </div>
+    
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
